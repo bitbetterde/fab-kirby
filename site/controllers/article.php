fcc44cf7c1bcb894335f870a551b999e26d1d283
Kilian <kilian.krackhardt@haw-hamburg.de>
2025-02-28 16:39:15 +0100

    Add article controller with breadcrumbs

---
+++ site/controllers/article.php
@@ -0,0 +1,42 @@
+<?php
+
+use Kirby\Cms\Page;
+use Kirby\Cms\Site;
+use tobimori\Inertia\Inertia;
+
+return function (Page $page, Site $site) {
+// 	var_dump($site->breadcrumb()->toArray());
+// 	print_r($site->breadcrumb()->toArray());
+
+
+function traverseMenu($item)
+{
+	if ($item->hasChildren()) {
+		$childrenItems = [];
+		foreach ($item->children()->listed() as $childItem) {
+			array_push($childrenItems, traverseMenu($childItem));
+		}
+		return ['title' => $item->title()->value(), 'target' => $item->url(), 'children' => $childrenItems];
+	} else {
+		return ['title' => $item->title()->value(), 'target' => $item->url()];
+	}
+}
+
+	return Inertia::createResponse(
+		$page->intendedTemplate(),
+		[
+			'page' => $page->toArray(),
+			'menu' => traverseMenu($site),
+
+            'breadcrumbs' => $site->breadcrumb()->map(function ($crumb) {
+                return [
+                    'target' => $crumb->url(),
+                    'title'  => $crumb->title()->value
+                ];
+            })->values()
+
+
+		]
+	);
+
+};
