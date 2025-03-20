c1eb5343541ecca3af77982d922326dff5650784
Kilian <kille.k737@gmail.com>
2025-03-20 10:28:44 +0100

    Add actionbox block

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -8,16 +8,18 @@ import "@fontsource/ibm-plex-sans";
 import "../base.css";
 
 import {
-    Article,
-    Footer,
-    Header,
-    HorizontalNewsCard,
-    Image,
-    Person,
-    YoutubeEmbed,
+  Article,
+  Footer,
+  Header,
+  HorizontalNewsCard,
+  Image,
+  Person,
+  YoutubeEmbed,
+  ActionBox,
+  Button,
 } from "@fchh/fcos-suite-ui";
-import {Home} from "@carbon/icons-react";
-import {CarbonIcon} from "../components/CarbonIcon";
+import { Home } from "@carbon/icons-react";
+import { CarbonIcon } from "../components/CarbonIcon";
 
 // interface ArticleTemplateProps {
 //   toolbar: IToolbarItem[];
@@ -26,114 +28,131 @@ import {CarbonIcon} from "../components/CarbonIcon";
 
 // The article template is for any kind of "article" like content. Could be a blog post, a news post or an event. It is not intended as home page.
 export default (props) => {
-    console.log("Article Template", props);
+  console.log("Article Template", props);
 
-    const blocks = props?.page?.content?.text ?? [];
+  const blocks = props?.page?.content?.text ?? [];
 
-    return (
-        <div>
-            <Header
-                topBarItems={props?.menu.children}
-                toolBarItems={props?.toolbar?.map((item) => ({
-                    ...item,
-                    icon: (
-                        <CarbonIcon
-                            name={item.icon}
-                            className="size-6 sm:size-5 sm:mr-2 mb-1 sm:mb-0"
-                        />
-                    ),
-                }))}
-                hideSearchIcon
+  return (
+    <div>
+      <Header
+        topBarItems={props?.menu.children}
+        toolBarItems={props?.toolbar?.map((item) => ({
+          ...item,
+          icon: (
+            <CarbonIcon
+              name={item.icon}
+              className="size-6 sm:size-5 sm:mr-2 mb-1 sm:mb-0"
             />
-            <Article
-                titleImage={props?.heroimage?.url}
-                titleImageAlt={props?.heroimage?.alt}
-                imageTag={props?.heroimage?.credits}
-                breadcrumbs={props?.breadcrumbs.map((crumb, i) =>
-                    i == 0 ? {...crumb, icon: <Home className="size-4"/>} : crumb
-                )}
-                hideFooterSeparator={true}
-                title={props?.page?.content.title}
-                subtitle={props?.page?.content.subheading}
-                teaser={props?.page.content.teaser}
-            >
-                {blocks?.map((block) => {
-                    if (block.type === "person") {
-                        return (
-                            <div className="grid grid-cols-3 py-8">
-                                {block.content?.people?.map((person) => (
-                                    <Person
-                                        key={person.name}
-                                        image={person.image?.[0]?.url}
-                                        name={person.name}
-                                        position={person.position}
-                                        organization={person.organisation}
-                                        facebook={person.facebook}
-                                        instagram={person.instagram}
-                                        mastodon={person.mastodon}
-                                        twitter={person.twitter}
-                                        xing={person.xing}
-                                        linkedin={person.linkedin}
-                                        email={person.email}
-                                    />
-                                ))}
-                            </div>
-                        );
-                    } else if (block.type === "heading") {
-                        const HeadlineTag = block.content.level;
-                        return <HeadlineTag>{block.content.text}</HeadlineTag>;
-                    } else if (block.type === "image") {
-                        return (
-                            <>
-                                <pre>{JSON.stringify(block, null, 2)}</pre>
-                                <Image
-                                    src={block.content.image?.[0]?.url}
-                                    alt={block.content.alt}
-                                    caption={block.content.caption}
-                                    subCaption={block.content.subcaption}
-                                    tag={block.content.tag}
-                                />
-                            </>
-                        );
-                    } else if (block.type === "code") {
-                        return (
-                            <pre>
+          ),
+        }))}
+        hideSearchIcon
+      />
+      <Article
+        titleImage={props?.heroimage?.url}
+        titleImageAlt={props?.heroimage?.alt}
+        imageTag={props?.heroimage?.credits}
+        breadcrumbs={props?.breadcrumbs.map((crumb, i) =>
+          i == 0 ? { ...crumb, icon: <Home className="size-4" /> } : crumb,
+        )}
+        hideFooterSeparator={true}
+        title={props?.page?.content.title}
+        subtitle={props?.page?.content.subheading}
+        teaser={props?.page.content.teaser}
+      >
+        {blocks?.map((block) => {
+          if (block.type === "person") {
+            return (
+              <div className="grid grid-cols-3 py-8">
+                {block.content?.people?.map((person) => (
+                  <Person
+                    key={person.name}
+                    image={person.image?.[0]?.url}
+                    name={person.name}
+                    position={person.position}
+                    organization={person.organisation}
+                    facebook={person.facebook}
+                    instagram={person.instagram}
+                    mastodon={person.mastodon}
+                    twitter={person.twitter}
+                    xing={person.xing}
+                    linkedin={person.linkedin}
+                    email={person.email}
+                  />
+                ))}
+              </div>
+            );
+          } else if (block.type === "heading") {
+            const HeadlineTag = block.content.level;
+            return <HeadlineTag>{block.content.text}</HeadlineTag>;
+          } else if (block.type === "image") {
+            return (
+              <>
+                <pre>{JSON.stringify(block, null, 2)}</pre>
+                <Image
+                  src={block.content.image?.[0]?.url}
+                  alt={block.content.alt}
+                  caption={block.content.caption}
+                  subCaption={block.content.subcaption}
+                  tag={block.content.tag}
+                />
+              </>
+            );
+          } else if (block.type === "code") {
+            return (
+              <pre>
                 <code className={`language-${block.content.language}`}>
                   {block.content.code}
                 </code>
               </pre>
-                        );
-                    } else if (block.type === "youtube") {
-                        return (
-                            <YoutubeEmbed videoId={block.content.videoid} title="Hallo"/>
-                        );
-                    } else if (block.type === "horizontalcard") {
-                        console.log('test123', block.content.target.content)
-                        return (
-                            <HorizontalNewsCard
-                                className="fs-not-prose"
-                                variant="dark"
-                                title={block.content.target.content.title}
-                                category={{id: 1, title: "Seite"}}
-                                description={block.content.target.content.teaser}
-                                href={block.content.target.url}
-                                image={{
-                                    alt: block.content.target.content.heroimage?.name,
-                                    src: block.content.target.content.heroimage?.url,
-                                }}
-                            />
-                        );
-                    }
+            );
+          } else if (block.type === "youtube") {
+            return (
+              <YoutubeEmbed videoId={block.content.videoid} title="Hallo" />
+            );
+          } else if (block.type === "actionbox") {
+            return (
+              <ActionBox
+                title={block.content.title}
+                description={block.content.description}
+              >
+                {block.content.buttons?.map((button, index) => (
+                  <Button
+                    key={index}
+                    href={button.target}
+                    text={button.caption}
+                    size="md"
+                    newTab={true}
+                  />
+                ))}
+              </ActionBox>
+            );
+          } else if (block.type === "horizontalcard") {
+            console.log("test123", block.content.target.content);
+            return (
+              <HorizontalNewsCard
+                className="fs-not-prose"
+                variant="dark"
+                title={block.content.target.content.title}
+                category={{ id: 1, title: "Seite" }}
+                description={block.content.target.content.teaser}
+                href={block.content.target.url}
+                image={{
+                  alt: block.content.target.content.heroimage?.name,
+                  src: block.content.target.content.heroimage?.url,
+                }}
+              />
+            );
+          }
 
-                    return (
-                        <div
-                            key={block.id}
-                            dangerouslySetInnerHTML={{__html: block.content.text}}
-                        ></div>
-                    );
-                })}
-            </Article>
-            <Footer menu={props?.menu.children}/>
-        </div>
-    );
+          return (
+            <div
+              key={block.id}
+              dangerouslySetInnerHTML={{ __html: block.content.text }}
+            ></div>
+          );
+        })}
+      </Article>
+      <Footer menu={props?.menu.children} />
+    </div>
+  );
 };
