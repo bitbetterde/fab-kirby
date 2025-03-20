2aeb2ea867b0f56d16017e2d85737bf6c60ba2d1
Moritz Stückler <moritz@bitbetter.de>
2025-03-20 08:03:07 -0400

    feat: add initial map setup

---
+++ site/controllers/map.php
@@ -0,0 +1,38 @@
+<?php
+
+use Kirby\Cms\Page;
+use Kirby\Cms\Site;
+use tobimori\Inertia\Inertia;
+
+// Build data structure for Breadcrumbs navigation
+function traverseMenu($item)
+{
+    if ($item->hasChildren()) {
+        $childrenItems = [];
+        foreach ($item->children()->listed() as $childItem) {
+            array_push($childrenItems, traverseMenu($childItem));
+        }
+        return ['title' => $item->title()->value(), 'target' => $item->url(), 'children' => $childrenItems];
+    } else {
+        return ['title' => $item->title()->value(), 'target' => $item->url()];
+    }
+}
+
+function getDefaultInertiaProps(Page $page, Site $site)
+{
+    $pageArr = $page->toArray();
+
+    return [
+        'page' => $pageArr,
+        'menu' => traverseMenu($site),
+        'toolbar' => $site->content()->hideToolbar()->toBool() ? null : $site->content()->toolbar()->toStructure()->toArray(),
+    ];
+}
+
+
+return function (Page $page, Site $site) {
+    return Inertia::createResponse(
+        $page->intendedTemplate(),
+        getDefaultInertiaProps($page, $site),
+    );
+};
